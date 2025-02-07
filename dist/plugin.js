31745ac17b4a7cb209fbfc81b152802f077e5cf5
xclas721 <xclas721@gmail.com>
2025-02-07 17:21:47 +0000

    new js

---
+++ dist/plugin.js
@@ -0,0 +1,7 @@
+exports.version = 1.0
+exports.description = "Light/Dark theme for HFS"
+exports.apiRequired = 1
+exports.repo = "xclas721/hfs-gpt-style-theme"
+exports.frontend_css = 'style.css'
+exports.isTheme = true
+exports.preview = ["https://github.com/user-attachments/assets/c34327d2-a461-4c3c-9abe-8a5133042626"]
